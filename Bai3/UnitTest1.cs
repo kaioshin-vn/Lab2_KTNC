156954baac0ac4671e4472d859bec0a29e6c3caa
SuRinRin <dangnguyen300708@gmail.com>
2024-03-12 13:53:27 +0700

    hhh

--- Bai3/UnitTest1.cs
+++ Bai3/UnitTest1.cs
@@ -1,42 +1,94 @@
+using NUnit.Framework.Internal;
+
 namespace Bai3
 {
-    public class GiaiThua
-    {
-        public int TinhGiaiThua (int n)
-        {
-            try
-            {
-                if (n < 0)
-                {
-                    return -1;
-                }
-                int result = 1;
-                for (int i = 1; i <= n; i++)
-                {
-                    result *= i;
-                }
-                return result;
-            }
-             catch (Exception e)
-            {
-                return -1;
-            }
-            
-        }
-}
-    public class Tests
-    {
-        GiaiThua giaiThua;
-        [SetUp]
-        public void Setup()
-        {
-            giaiThua = new GiaiThua();
-        }
+	public class GiaiThua
+	{
+		public int TinhGiaiThua(int n)
+		{
+			try
+			{
+				if (n < 0|| n == int.MaxValue)
+				{
+					return -1;
+				}
+				int result = 1;
+				for (int i = 1; i <= n; i++)
+				{
+					result *= i;
+				}
+				if (result == 0)
+				{
+					return -1;
+				}
+				return result;
+			}
+			catch (Exception e)
+			{
+				return -1;
+			}
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
-    }
+		}
+	}
+	public class Tests
+	{
+		GiaiThua _giaiThua;
+		[SetUp]
+		public void Setup()
+		{
+			_giaiThua = new GiaiThua();
+		}
+
+
+		[Test]
+		public void TC_Bien1()
+		{
+			Assert.That(_giaiThua.TinhGiaiThua(-1), Is.EqualTo(-1));
+		}
+		[Test]
+		public void TC_Bien2()
+		{
+			Assert.That(_giaiThua.TinhGiaiThua(0), Is.EqualTo(1));
+		}
+		[Test]
+		public void TC_Bien3()
+		{
+			Assert.That(_giaiThua.TinhGiaiThua(1), Is.EqualTo(1));
+		}
+		[Test]
+		public void TC_Bien4()
+		{
+			Assert.That(_giaiThua.TinhGiaiThua(int.MaxValue - 1), Is.EqualTo(-1));
+		}
+		[Test]
+		public void TC_Bien5()
+		{
+			Assert.That(_giaiThua.TinhGiaiThua(int.MaxValue), Is.EqualTo(-1));
+		}
+		[Test]
+		public void TC_TuongDuong1()
+		{
+			Assert.That(_giaiThua.TinhGiaiThua(5), Is.EqualTo(120));
+		}
+		[Test]
+		public void TC_TuongDuong2()
+		{
+			Assert.That(_giaiThua.TinhGiaiThua(-5), Is.EqualTo(-1));
+		}
+		[Test]
+		public void TC_TuongDuong3()
+		{
+			Assert.That(_giaiThua.TinhGiaiThua(-75), Is.EqualTo(-1));
+		}
+		[Test]
+		public void TC_TuongDuong4()
+		{
+			Assert.That(_giaiThua.TinhGiaiThua(665), Is.EqualTo(-1));
+		}
+		[Test]
+		public void TC_TuongDuong5()
+		{
+			Assert.That(_giaiThua.TinhGiaiThua(50), Is.EqualTo(-1));
+		}
+	}
 }
