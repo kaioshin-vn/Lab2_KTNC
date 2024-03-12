2183369353b32efa8273fb38a2f23e9f8468c4da
phongke004 <phongx543@gmail.com>
2024-03-12 22:35:07 +0700

    ahahaha

--- Bai1/UnitTest1.cs
+++ Bai1/UnitTest1.cs
@@ -28,47 +28,47 @@ namespace Bai1
 		[Test]
 		public void TestTuongDuong5()
 		{
-			Assert.That(_sumT.Tong(14, 1), Is.EqualTo(15));
+			Assert.That(_sumT.Tong(8, 1), Is.EqualTo(9));
 		}
 		[Test]
 		public void TestTuongDuong4()
 		{
-			Assert.That(_sumT.Tong(6, 4), Is.EqualTo(10));
+			Assert.That(_sumT.Tong(1, 6), Is.EqualTo(7));
 		}
 		[Test]
 		public void TestTuongDuong3()
 		{
-			Assert.That(_sumT.Tong(3, 3), Is.EqualTo(6));
+			Assert.That(_sumT.Tong(9, 8), Is.EqualTo(17));
 		}
 		[Test]
 		public void TestTuongDuong2()
 		{
-			Assert.That(_sumT.Tong(3, 2), Is.EqualTo(5));
+			Assert.That(_sumT.Tong(2, 1), Is.EqualTo(3));
 		}
 		[Test]
 		public void TestBien1()
 		{
-			Assert.That(_sumT.Tong(0, 1), Is.EqualTo(1));
+			Assert.That(_sumT.Tong(5, 9), Is.EqualTo(14));
 		}
 		[Test]
 		public void TestBien2()
 		{
-			Assert.That(_sumT.Tong(0, -1), Is.EqualTo(-1));
+			Assert.That(_sumT.Tong(1, 5), Is.EqualTo(6));
 		}
 		[Test]
 		public void TestBien3()
 		{
-			Assert.That(_sumT.Tong(9, 1), Is.EqualTo(10));
+			Assert.That(_sumT.Tong(9,2), Is.EqualTo(11));
 		}
 		[Test]
 		public void TestBien4()
 		{
-			Assert.That(_sumT.Tong(11, -1), Is.EqualTo(10));
+			Assert.That(_sumT.Tong(7, 1), Is.EqualTo(8));
 		}
 		[Test]
 		public void TestBien5()
 		{
-			Assert.That(_sumT.Tong(-1, 9), Is.EqualTo(8));
+			Assert.That(_sumT.Tong(4, 9), Is.EqualTo(13));
 		}
 	}
 }
