96b1e0424b8aecc8b11a2c36ab83ed8a876d537e
Dang Giap <dgiap540@gmail.com>
2024-03-12 13:28:12 +0700

    ttt

--- Bai3/UnitTest1.cs
+++ Bai3/UnitTest1.cs
@@ -4,20 +4,33 @@ namespace Bai3
     {
         public int TinhGiaiThua (int n)
         {
-            int result = 1;
-            for (int i = 1; i <= n; i++)
+            try
             {
-                result *= i;
-            } 
-            return result;
-
+                if (n < 0)
+                {
+                    return -1;
+                }
+                int result = 1;
+                for (int i = 1; i <= n; i++)
+                {
+                    result *= i;
+                }
+                return result;
+            }
+             catch (Exception e)
+            {
+                return -1;
+            }
+            
         }
 }
     public class Tests
     {
+        GiaiThua giaiThua;
         [SetUp]
         public void Setup()
         {
+            giaiThua = new GiaiThua();
         }
 
         [Test]
