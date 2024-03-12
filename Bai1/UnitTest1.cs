9f0f0b783b8774565943ece6c1aa4709ed8d19a2
SuRinRin <dangnguyen300708@gmail.com>
2024-03-12 14:01:57 +0700

    ef

--- Bai1/UnitTest1.cs
+++ Bai1/UnitTest1.cs
@@ -1,16 +1,74 @@
 namespace Bai1
 {
-    public class Tests
-    {
-        [SetUp]
-        public void Setup()
-        {
-        }
+	public class Tong2So
+	{
+		public int Tong(int a, int b)
+		{
+			return a + b;
+		}
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
-    }
+	}
+	[TestFixture]
+
+	public class Tests
+	{
+		private Tong2So _sumT;
+		[SetUp]
+		public void Setup()
+		{
+			_sumT = new Tong2So();
+		}
+
+		[Test]
+		public void TestTuongDuong1()
+		{
+			Assert.AreEqual(5, _sumT.Tong(4, 1));
+		}
+
+		[Test]
+		public void TestTuongDuong5()
+		{
+			Assert.AreEqual(5, _sumT.Tong(4, 1));
+		}
+		[Test]
+		public void TestTuongDuong4()
+		{
+			Assert.AreEqual(10, _sumT.Tong(6, 4));
+		}
+		[Test]
+		public void TestTuongDuong3()
+		{
+			Assert.AreEqual(6, _sumT.Tong(3, 3));
+		}
+		[Test]
+		public void TestTuongDuong2()
+		{
+			Assert.AreEqual(5, _sumT.Tong(3, 2));
+		}
+		[Test]
+		public void TestBien1()
+		{
+			Assert.AreEqual(1, _sumT.Tong(0, 1));
+		}
+		[Test]
+		public void TestBien2()
+		{
+			Assert.AreEqual(-1, _sumT.Tong(0, -1));
+		}
+		[Test]
+		public void TestBien3()
+		{
+			Assert.AreEqual(10, _sumT.Tong(9, 1));
+		}
+		[Test]
+		public void TestBien4()
+		{
+			Assert.AreEqual(10, _sumT.Tong(11, -1));
+		}
+		[Test]
+		public void TestBien5()
+		{
+			Assert.AreEqual(8, _sumT.Tong(-1, 9));
+		}
+	}
 }
